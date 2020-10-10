6c5528f3d8ea7c54215a1ea164d0005bd3deaabc
pratikshatamadalge <pratikshatamadalge21@gmail.com>
2020-10-10 10:32:50 +0530

    DELETED:Unwanted packages

--- GoogleSearchAzureTest/TestCases/GoogleTest.cs
+++ GoogleSearchAzureTest/TestCases/GoogleTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GoogleSearchAzureTest.Base;
+﻿using GoogleSearchAzureTest.Base;
 using GoogleSearchAzureTest.Pages;
 using NUnit.Framework;
 
